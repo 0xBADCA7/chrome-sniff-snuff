d4be5554da3fa854995feb255611cfd71e28ee36
0xBADCA7/Netcat <0xBADCA7@users.github.com>
2015-12-23 00:47:02 +0100

    Update release

--- src/create_sniffsnuff.js
+++ src/create_sniffsnuff.js
@@ -1,19 +1,11 @@
 chrome.devtools.panels.create(
-    "SniffSnuff", 
-    "icon16.png", 
+    "SniffSnuff",
+    "icons/icon48.png",
     "sniffsnuff.html",
     function()
     {
- 	  /* stuff */
-    }
-);
-
-
-chrome.devtools.panels.elements.createSidebarPane("SniffSnuff",
-    function(sidebar)
-    {
-        //sidebar.setPage("sniffsnuff.html");
-        sidebar.setObject({ someData: "Some data to show", b: "zzz" });
-        //sidebar.setExpression(window);
+      /*
+        Icon made by Freepik. Licensed by CC BY 3.0
+      */
     }
 );
